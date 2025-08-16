3a527fc5ff3011d885ba296aa46902cdb4431d4b
Silver <silver@squirl.dev>
2025-08-16 06:03:02 +0100

    make update script sequential
    the parallelism was causing bugs and i could not be bothered to fix it

--- update-feeds.sh
+++ update-feeds.sh
@@ -2,9 +2,7 @@
 #!nix shell -i --inputs-from . nixpkgs#bash nixpkgs#busybox nixpkgs#curl nixpkgs#nix nixpkgs#alejandra -c bash
 # shellcheck shell=bash disable=SC2266
 set -e
-trap wait INT TERM
 
-max_jobs=16
 out=feeds.nix
 tarball=https://github.com/Kapeli/feeds/archive/refs/heads/master.tar.gz
 feed_base_url=https://kapeli.com/feeds
@@ -14,9 +12,6 @@ fetch_feed_inner() {
     url=$feed_base_url/$name.tgz
     shasum=$(nix-prefetch-url --unpack --type sha256 "$url" 2>/dev/null) || return
     hash=$(nix-hash --to-sri --type sha256 "$shasum") || return
-
-    exec >>"$out"
-    flock 1
     printf '"%s" = { url = "%s"; hash = "%s"; };\n' "$name" "$url" "$hash"
 }
 fetch_feed() {
@@ -28,21 +23,14 @@ mapfile -t filenames < <(curl -sL "$tarball" | tar tz | sed -E 's/.*\/([^.]+)\.x
 
 finished=0
 status() {
-    jobs -p | printf '\r\e[K  %d+%d/%d\r' "$finished" "$(wc -l)" "${#filenames[@]}" >&2
+    printf '\r\e[K  %d/%d\r' "$finished" "${#filenames[@]}" >&2
 }
 
-: >"$out"
 for name in "${filenames[@]}"; do
     status
-    while jobs -p | [ "$(wc -l)" -ge "$max_jobs" ]; do
-        wait -n
-        ((finished += 1))
-    done
-    fetch_feed "$name" &
-done
-status
-
-wait
+    fetch_feed "$name"
+    ((finished += 1))
+done >"$out"
 status
 echo
 
