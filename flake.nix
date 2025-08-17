2612dcf5d963ceed8de54dc3e40d4adc9cc8de28
Silver <silver@squirl.dev>
2025-08-17 11:25:18 +0100

    support "ZDash" docsets
    that's what zeal calls them internally, i have no idea if they have an
    official name or not

--- flake.nix
+++ flake.nix
@@ -11,20 +11,56 @@
           src = pkgs.fetchzip {inherit (info) url hash;};
           inherit (info) extra;
           nativeBuildInputs = [pkgs.sqlite];
-          buildCommand = ''
-            mkdir -p "$out"
-            echo "Copying docset"
-            cp -rT "$src" "$out"
-            echo "Adding icons and metadata"
-            cp -rT "$extra" "$out"
-            echo "Creating search index"
-            cp "$src/Contents/Resources/docSet.dsidx" docSet.dsidx
-            chmod +w docSet.dsidx
-            sqlite3 docSet.dsidx '
-              CREATE INDEX IF NOT EXISTS __zi_name0001 ON searchIndex (name COLLATE NOCASE);
-            '
-            chmod +w "$out/Contents/Resources/docSet.dsidx"
-            cp docSet.dsidx "$out/Contents/Resources/"
+
+          buildPhase = ''
+            runHook preBuild
+
+            echo 'Copy icons and metadata'
+            cp -rT "$extra" .
+
+            echo 'Create search index'
+            db=Contents/Resources/docSet.dsidx
+            #chmod +w "$db"
+            hasSearchIndex=$(sqlite3 "$db" '
+              SELECT 1 FROM sqlite_master WHERE type="table" AND name="searchIndex" COLLATE NOCASE;
+            ')
+            if [ -n "$hasSearchIndex" ]; then
+              sqlite3 "$db" '
+                CREATE INDEX IF NOT EXISTS __zi_name0001 ON searchIndex (name COLLATE NOCASE);
+              '
+            else
+              sqlite3 "$db" '
+                --CREATE INDEX IF NOT EXISTS __zi_name0001 ON ztoken (ztokenname COLLATE NOCASE);
+                CREATE VIEW IF NOT EXISTS searchIndex AS
+                  SELECT
+                    ztokenname AS name,
+                    ztypename AS type,
+                    zpath AS path,
+                    zanchor AS fragment
+                  FROM ztoken
+                  INNER JOIN ztokenmetainformation
+                    ON ztoken.zmetainformation = ztokenmetainformation.z_pk
+                  INNER JOIN zfilepath
+                    ON ztokenmetainformation.zfile = zfilepath.z_pk
+                  INNER JOIN ztokentype
+                    ON ztoken.ztokentype = ztokentype.z_pk;
+              '
+            fi
+
+            runHook postBuild
+          '';
+
+          doCheck = true;
+          checkPhase = ''
+            runHook preCheck
+            sqlite3 "$db" 'SELECT * FROM searchIndex LIMIT 1;'
+            runHook postCheck
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            cp -r . "$out"
+            runHook postInstall
           '';
         };
     in
